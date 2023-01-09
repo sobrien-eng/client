db3693bb579575e22642cbc6b9d52744680e7acc
sobrien-eng <sobrien@student.neumont.edu>
2023-01-09 12:56:41 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+// import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import {useDispatch} from 'react-redux';
+import {getPosts} from './actions/posts';
+// import memories from "./images/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+// import useStyles from "./styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() =>{
+        dispatch(getPosts());
+    }, [dispatch]);
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+
+    )
 }
 
 export default App;
